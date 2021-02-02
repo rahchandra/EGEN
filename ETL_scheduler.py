338fb8ed2ff0fa01e95ab109021b79585c02184c
Rahul Chandra <chandra.ra@husky.neu.edu>
2021-02-02 14:58:14 -0500

    Update ETL_scheduler.py

--- ETL_scheduler.py
+++ ETL_scheduler.py
@@ -9,8 +9,8 @@ def job(isHistoric):
 
 
 isHistoric = True
-schedule.every(10).seconds.do(job,isHistoric)
-# schedule.every().day.at("09:00").do(job,isHistoric)
+# schedule.every(10).seconds.do(job,isHistoric)
+schedule.every().day.at("09:00").do(job,isHistoric)
 
 while True:
     schedule.run_pending()
